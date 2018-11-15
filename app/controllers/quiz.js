6509ec1c571e13f588ee353e5a86ce4d9c9de50e
Chris Albanese <Christopher@CafeRE.com>
2018-11-15 11:49:39 -0500

    new branch no component

--- app/controllers/quiz.js
+++ app/controllers/quiz.js
@@ -1,5 +1,92 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
- 
+  selectedA: false,
+  selectedB: false,
+  selectedC: false,
+  selectedD: false,
+  currentSelection: "",
+  correctAnswer: "",
+  currentNum: 2,
+  totalQuestions: 0,
+  totalCorrect: 0,
+  totalWrong: 0,
+
+  
+  actions: {
+    selectAnswer(id, correctAnswer) {
+      const target = event.target.id;
+
+      this.set("selectedA", false);
+      this.set("selectedB", false);
+      this.set("selectedC", false);
+      this.set("selectedD", false);
+
+      console.log("this id selected", id, "selected target", target);
+
+      switch (target) {
+        case "A":
+          return this.setProperties({ selectedA: true, currentSelection: "A" });
+        case "B":
+          return this.setProperties({ selectedB: true, currentSelection: "B" });
+        case "C":
+          return this.setProperties({ selectedC: true, currentSelection: "C" });
+        default:
+          return this.setProperties({ selectedD: true, currentSelection: "D" });
+      }
+    },
+
+    closeModal(id) {
+      const closeModal = document.getElementById(id);
+      closeModal.classList.add("modal-hide");
+    },
+
+    nextQuestion(id) {
+      console.log("currentNum", this.currentNum, "totalQuesitons", this.totalQuestions );
+      this.actions.closeModal(id);
+      if (this.currentNum < this.totalQuestions) {
+        this.set('currentNum', this.currentNum + 1)
+      } else {
+        completeModal.classList.remove("modal-hide");
+        completeModal.classList.add("modal-show");
+      }
+    },
+
+    resetQuiz(id) {
+      this.actions.closeModal(id);
+      this.setProperties({currentNum: 1, currentSelection: ""})
+    },
+
+    submitAnswer(correctAnswer, question) {
+      const correct = correctAnswer.toLowerCase();
+      const selected = this.currentSelection.toLowerCase();
+
+      switch (correct) {
+        case "a":
+          this.set("correctAnswer", `A: ${question.answerA}`);
+          break;
+        case "b":
+          this.set("correctAnswer", `B: ${question.answerB}`);
+          break;
+        case "c":
+          this.set("correctAnswer", `C: ${question.answerC}`);
+          break;
+        default:
+          this.set("correctAnswer", `D: ${question.answerD}`);
+          break;
+      }
+
+      const successModal = document.getElementById("successModal");
+      const failModal = document.getElementById("failModal");
+
+      if (correct === selected) {
+        this.set
+        successModal.classList.remove("modal-hide");
+        successModal.classList.add("modal-show");
+      } else {
+        failModal.classList.remove("modal-hide");
+        failModal.classList.add("modal-show");
+      }
+    }
+  }
 });
--- app/routes/quiz.js
+++ app/routes/quiz.js
@@ -2,27 +2,25 @@ import Route from "@ember/routing/route";
 
 
 export default Route.extend({
-  quizLength: 0,
-  currentQuestion: 0,
-  
-
-
   model() {
     // const data = this.get('question-view')
     // console.log('data', data);
     
-    
-
+    let currentQ = this.controllerFor('quiz').currentNum -1
+    console.log('currentQ', currentQ);
+    console.log("controller", this.controllerFor('quiz'));
     let quizArrIds = [];
     return this.store
       .findAll("question")
-      .then(allQs =>
+      .then(allQs => 
         allQs.map(question => {
           quizArrIds.push(question.id);
         })
       )
       .then(() => {
-        return this.store.findRecord("question", quizArrIds[1]);
+        
+        this.controllerFor("quiz").set("totalQuestions", quizArrIds.length);
+        return this.store.findRecord("question", quizArrIds[currentQ]);
       });
 
     // this.store.findRecord('question', quizArrIds[this.currentQuestion])
